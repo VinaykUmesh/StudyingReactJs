5e831922f1ee377008a20758818c49729fababc8
Vinayk-Umesh09 <44252966+Vinayk-Umesh09@users.noreply.github.com>
2019-02-27 22:26:23 +0530

    Exercise 1 & 2

---
+++ Es6/exercise.js
@@ -0,0 +1,8 @@
+const name = "James";
+const empid = 25;
+const eyes =" blue";
+let  age  = 25;
+let  position = "Developer";
+let salary = 3500;
+let knowledge = ['javascirpt','react'] ;
+let married = false;
