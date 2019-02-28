36a05e4375e2ccc8d09a7c015e94b3b9a1d416b3
VinaykUmesh <luxurygeek.umesh@gmail.com>
2019-02-28 11:54:00 +0530

    Template Strings

---
+++ Es6/TemplateStrings.js
@@ -0,0 +1,18 @@
+function templateStrings(){
+
+    const name = "umesh";
+    const age = 23;
+    const position = "FullStack Developer";
+
+    // In ES5
+    //console.log(" His name is  "+name+ " and he is " +age+ " years old  and he is a " +position   )
+
+     console.log('His name is ${name} and he is ${age} years old and he is a ${position}')
+     
+
+     const names = ["Umesh","deepan","vivek"];
+
+     console.log('The name is ${names[0]}')
+
+}
+templateStrings();
