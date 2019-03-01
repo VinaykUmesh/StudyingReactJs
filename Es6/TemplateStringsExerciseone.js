4128847efe54e9493458cf1a910e2b9085347c1f
Vinayk-Umesh09 <44252966+Vinayk-Umesh09@users.noreply.github.com>
2019-03-01 05:31:22 +0530

    Template String Exercise one

---
+++ Es6/TemplateStringsExerciseone.js
@@ -0,0 +1,6 @@
+function Employee(name,lastname,age,position,salary){
+
+ console.log(`My name is ${name} ${lastname},  i'm ${age} years old. i work as ${position} and make $ ${salary}`)
+
+}
+Employee('Vinayk','Umesh',23,'Full Stack Developer',2000)
