dca5bb64f06cf9a472533e36951245775686c22d
Vinayk-Umesh09 <44252966+Vinayk-Umesh09@users.noreply.github.com>
2019-03-01 06:02:32 +0530

    Template STring Excercise two completed

--- Es6/TemplateStringsExerciseone.js
+++ Es6/TemplateStringsExerciseone.js
@@ -1,6 +1,6 @@
 function Employee(name,lastname,age,position,salary){
 
- console.log(`My name is ${name} ${lastname},  i'm ${age} years old. i work as ${position} and make $ ${salary}`)
+ console.log(`My name is ${name} ${lastname},  i'm ${age} years old. i work as ${position} and make $P${salary}`)
 
 }
 Employee('Vinayk','Umesh',23,'Full Stack Developer',2000)
