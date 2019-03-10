b735249e837ee89c0756d2138517d7cf324e1cfd
Vinayk Umesh <44252966+Vinayk-Umesh09@users.noreply.github.com>
2019-03-10 09:32:07 +0530

    Getting Dynamic Data from a Function or user object.

--- React/React Basics/myawesomeapp/src/index.js
+++ React/React Basics/myawesomeapp/src/index.js
@@ -5,11 +5,18 @@ import ReactDOM from 'react-dom';
 //Assets
 import Header from './assets/header'
 
+//Fetching Dynamic Data form the user object 
+const user = {
+    fname : 'Vinayk',
+    lname : 'Umesh',
+    age : 23
+}
 
 const App = () => {
          return (
-          <div>
-              <Header/>
+              <div>
+                  <div><Header/></div>
+                  <div>This is {user.fname} {user.lname} ,and Im {user.age} years old</div>
               </div>
           )
 }
