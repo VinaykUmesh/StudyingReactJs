bcb36f1c170d9662dd6fe0a04cd20f7c1b722f34
Vinayk Umesh <44252966+Vinayk-Umesh09@users.noreply.github.com>
2019-03-11 22:28:44 +0530

    React Events

--- React/React Basics/myawesomeapp/src/assets/header.js
+++ React/React Basics/myawesomeapp/src/assets/header.js
@@ -2,11 +2,16 @@ import React,{ Component } from 'react';
 import '../css/styles.css'
 
 class Header extends Component {
+
+  oninputclick (event) {
+    console.log(event.target.value)
+
+  }
             render(){
                  return (
                <header >
-               <div className="logo">LOGO</div>
-               <div ><input type="text"/></div>
+               <div className="logo" >LOGO</div>
+               <div ><input type="text"   onChange={(e) => this.oninputclick(e)}/></div>
                </header>
             )
            }
