d989392102ea23ea3ca6d3590e53c85866e4c16c
VinaykUmesh <luxurygeek.umesh@gmail.com>
2019-03-12 18:25:59 +0530

    React this super keyword and State sateless

--- React/React Basics/myawesomeapp/src/assets/header.js
+++ React/React Basics/myawesomeapp/src/assets/header.js
@@ -2,21 +2,31 @@ import React,{ Component } from 'react';
 import '../css/styles.css'
 
 class Header extends Component {
+   
+    state = {
+         keywords : 'Hello',
+         title : 'the Keywords are :'
+    }
 
-  oninputclick (event) {
-    console.log(event.target.value)
+  oninputclick  = (event)  => {
+    // console.log(event.target.value)
+    this.setState({
+         keywords: event.target.value
+    })
+
 
   }
             render(){
+              console.log(this.state.keywords)
                  return (
                <header >
                <div className="logo" >LOGO</div>
-               <div ><input type="text"   onChange={(e) => this.oninputclick(e)}/></div>
+               <div ><input type="text"   onChange={this.oninputclick}/></div>
+               <div className="logo">{this.state.title}</div>
+               <div className="logo">{this.state.keywords}</div>
                </header>
             )
            }
-          }
-
-
+}
 
 export default Header;
