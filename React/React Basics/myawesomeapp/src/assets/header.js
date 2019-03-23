b8fbc3cc1649c283cd56c64b22e3c0d3f8b7ce0a
Vinayk Umesh <44252966+Vinayk-Umesh09@users.noreply.github.com>
2019-03-23 11:01:20 +0530

    finally found the solution for dynamic class name changer

--- React/React Basics/myawesomeapp/src/assets/header.js
+++ React/React Basics/myawesomeapp/src/assets/header.js
@@ -1,30 +1,44 @@
 import React,{ Component } from 'react';
 import '../css/styles.css'
 
+
 class Header extends Component {
-  state ={
-     active:false,
-     kewords:''
-   }
 
-  oninputclick = (event) => {
-
-    const value = event.target.value === ' ' ? false : true ;
-    this.setState({
-      active : value,
-      kewords : ' event.target.value'
-    });
-
-
+  state={
+   
+   keyword : 'active'
   }
-            render(){
+
+  inputchangehandler = (event) => {
+         const value = event.target.value.length>0 ? 'active' : 'non-active';
+           this.setState({
+            active: value,
+            keyword :value
+           });
+  }
+
+             render(){
+
+              // const style = {
+              //  background : 'blue'
+              // }
+              // if(this.state.keyword.length>0){
+              //  style.background = 'red'
+              // }else{
+              //  style.background= 'blue'
+              // }
+
+
                  return (
-               <header style={{background:`${this.state.active ? 'red': 'blue'}`}}>
-               <div className="logo" >TIMES</div>
-               <div ><input type="text"   onChange={(e) => this.oninputclick(e)}/></div>
-               </header>
-            )
-  }
+                  <header className={this.state.keyword}>
+                   <div className="logo">Times</div>
+                       <input 
+                       type="text"
+                       onChange={this.inputchangehandler}
+                       />
+                  </header>
+                 )
+             }
 }
 
 
