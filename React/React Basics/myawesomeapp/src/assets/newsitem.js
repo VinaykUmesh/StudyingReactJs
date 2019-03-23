35747f7056b0989dc0910db28a3174bf30b55b9a
Vinayk Umesh <44252966+Vinayk-Umesh09@users.noreply.github.com>
2019-03-23 21:06:47 +0530

    commited for build and eject

--- React/React Basics/myawesomeapp/src/assets/newsitem.js
+++ React/React Basics/myawesomeapp/src/assets/newsitem.js
@@ -1,27 +1,11 @@
 import React from 'react';
-import { css, hover  } from 'glamor' ;
+
 
 const NewsItem = ({item}) => {
-
-  let news_item = css({
-      padding: '20px',
-      boxSizing: 'border-cricle',
-      borderBottom : '1px solid grey' ,
-      ':hover' : {
-        color : '#e84118'
-      },
-      '@media(max-width: 500px)':{
-        color : '#e84118'
-      }
-  })
-
-  let item_color = css({
-    background : '#ffe8e2'
-  })
-     
+ 
 
  return(
-   <div   className={ `${news_item} ${item_color}`}>
+   <div  >
              <h3>{item.title}</h3>
              <div>{item.feed}</div>
             </div>
