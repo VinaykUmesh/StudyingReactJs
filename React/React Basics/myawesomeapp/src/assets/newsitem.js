04d56a20e0d84b81516033b691c94c4619690556
Vinayk Umesh <44252966+Vinayk-Umesh09@users.noreply.github.com>
2019-03-23 11:33:05 +0530

    using glamor components for more on styles in css

--- React/React Basics/myawesomeapp/src/assets/newsitem.js
+++ React/React Basics/myawesomeapp/src/assets/newsitem.js
@@ -1,10 +1,27 @@
-import React from "react";
-
+import React from 'react';
+import { css, hover  } from 'glamor' ;
 
 const NewsItem = ({item}) => {
 
+  let news_item = css({
+      padding: '20px',
+      boxSizing: 'border-cricle',
+      borderBottom : '1px solid grey' ,
+      ':hover' : {
+        color : '#e84118'
+      },
+      '@media(max-width: 500px)':{
+        color : '#e84118'
+      }
+  })
+
+  let item_color = css({
+    background : '#ffe8e2'
+  })
+     
+
  return(
-   <div>
+   <div   className={ `${news_item} ${item_color}`}>
              <h3>{item.title}</h3>
              <div>{item.feed}</div>
             </div>
