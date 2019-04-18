ff944b8e2e2eb521faf74eadb79068840aa0d222
Vinayk Umesh <44252966+Vinayk-Umesh09@users.noreply.github.com>
2019-04-18 09:02:09 +0530

    Setting Home page

---
+++ React/nba_app/src/routes.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Switch , Route } from 'react-router-dom';
+
+import Home from './components/Home/home';
+
+class Routes extends Component {
+ render() {
+  return (
+   <div>
+         <Switch>
+               <Route path="/" exact component={Home}/>
+          </Switch>   
+   </div>
+  );
+ }
+}
+
+export default Routes;
