13e285ad96a8bede80b754f666043c07352c9f5e
Vinayk Umesh <44252966+Vinayk-Umesh09@users.noreply.github.com>
2019-04-19 13:04:44 +0530

    Layout and HnF

--- React/nba_app/src/routes.js
+++ React/nba_app/src/routes.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react';
 import { Switch , Route } from 'react-router-dom';
 
 import Home from './components/Home/home';
+import Layout from './hoc/Layout/layout';
 
 class Routes extends Component {
  render() {
   return (
    <div>
-         <Switch>
-               <Route path="/" exact component={Home}/>
-          </Switch>   
+         <Layout>
+                  <Switch>
+                        <Route path="/" exact component={Home}/>
+                  </Switch>   
+          </Layout>
    </div>
   );
  }
