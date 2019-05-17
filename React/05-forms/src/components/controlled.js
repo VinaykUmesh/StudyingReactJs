f3d25e30247b1815f319419afc7aa69c94374c79
VinaykUmesh <luxurygeek.umesh@gmail.com>
2019-05-17 22:17:55 +0530

    Forms with Controllled method

--- React/05-forms/src/components/controlled.js
+++ React/05-forms/src/components/controlled.js
@@ -3,13 +3,43 @@ import React, { Component } from 'react';
 class Controlled extends Component {
 
     state = {
-        
+        name : '',
+        lastname:''
     }
+
+    handleNameChange =(event) =>{
+            this.setState({
+                name:event.target.value
+            })
+    }
+
+    handleLastnameChange =(event) =>{
+            this.setState({
+                lastname:event.target.value
+            })
+          }
 
     render(){
         return(
-            <div>
-                Controlled
+            <div className="container">
+                <form>
+                    <div className="form_element">
+                        <label>Enter your Name:</label>
+                        <input
+                            type="text"
+                            onChange={this.handleNameChange}
+                            value={this.state.name}
+                        />
+                    </div>
+                    <div className="form_element">
+                        <label>Enter your Lastname:</label>
+                        <input
+                            type="text"
+                            onChange={this.handleLastnameChange}
+                            value={this.state.lastname}
+                        />
+                    </div>
+                </form>
             </div>
         )
     }
--- React/05-forms/src/components/user.js
+++ React/05-forms/src/components/user.js
@@ -3,13 +3,39 @@ import React, { Component } from 'react';
 class User extends Component {
 
     state = {
+        fromData:{
+            name :{
+                element : 'Name',
+                value: ' ',
+                label : true,
+                labelText : 'Name',
+                config : {
+                    name : 'name_input',
+                    type : 'text',
+                    placeholders : 'Enter Your Name'
+                } 
+            },
+            lastname :{
+                element : 'Lastame',
+                value: ' ',
+                label : true,
+                labelText : 'Lastname',
+                config : {
+                    name : 'name_input',
+                    type : 'text',
+                    placeholders : 'Enter Your LAstname'
+                } 
+            }
+        }
         
     }
 
     render(){
         return(
-            <div>
-                User
+            <div className="container"> 
+                <form onSubmit={this.submitForm}>
+                    
+                </form>
             </div>
         )
     }
